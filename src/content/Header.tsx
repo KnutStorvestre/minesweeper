41364b72c13f259e9869b1a496a407141ac6b5b9
Knut Storvestre <knutmathiasstorvestre@gmail.com>
2024-02-24 23:35:15 +0100

    Added timer and font for the numbers in the timer

--- src/content/Header.tsx
+++ src/content/Header.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./Header.css";
 
 interface HeaderProps {
@@ -8,6 +9,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({
   minesRemaining,
+  isGameStarted,
   isGameOver,
   onRestart,
 }) => {
@@ -15,10 +17,32 @@ const Header: React.FC<HeaderProps> = ({
     <div className="header-container">
       <div className="flags" />
       <div className="status" />
-      <div className="timer" />
+      <div className="timer">
+        <Timer />
+      </div>
     </div>
   );
 };
+
+function Timer() {
+  const [timer, setTimer] = useState(995);
+
+  useEffect(() => {
+    if (timer < 999) {
+      const interval = setInterval(() => {
+        setTimer((prev) => prev + 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [timer]);
+
+  const formattedTime = String(timer).padStart(3, "0");
+
+  return <span>{formattedTime}</span>;
+}
 
 export default Header;
 
